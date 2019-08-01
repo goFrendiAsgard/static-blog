6f6360b3c5f300192d2545aeaac137de540423b3
gofrendi <gofrendiasgard@gmail.com>
2019-08-01 08:09:59 +0700

    rebuilding site Thu 01 Aug 2019 08:09:59 AM WIB

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,8 @@ if [ -n "$*" ]; then
 	msg="$*"
 fi
 
+printf "\033[0;32mCommit and push static blog source...\033[0m\n"
+
 # Add changes to git.
 git add . -A
 git commit -m "$msg"
@@ -20,6 +22,8 @@ git push origin master
 
 # Go To Public folder
 cd public
+
+printf "\033[0;32mCommit and push public...\033[0m\n"
 
 # Add changes to git.
 git add .
